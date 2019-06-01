9b666d129c132eb43444b8d6919cf97095b9c0da
daivin <daivin_zou@163.com>
2019-06-01 15:42:29 +0800

    B1-0501

--- chapter-1-spring-boot-quickstart/src/main/java/demo/springboot/web/HelloBookController.java
+++ chapter-1-spring-boot-quickstart/src/main/java/demo/springboot/web/HelloBookController.java
@@ -14,6 +14,6 @@ public class HelloBookController {
 
     @RequestMapping(value = "/book/hello",method = RequestMethod.GET)
     public String sayHello() {
-        return "Hello，《Spring Boot 2.x 核心技术实战 - 上 基础篇》！";
+        return "Hello，更改B1-0501";
     }
 }
